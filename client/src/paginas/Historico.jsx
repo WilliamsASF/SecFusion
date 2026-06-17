70668f9f0f312ff70b6ec7b967575fc22896bfa0
clarinha <158226328+claramtenorio@users.noreply.github.com>
2026-06-17 15:38:58 -0300

    Create Historico.jsx

---
+++ client/src/paginas/Historico.jsx
@@ -0,0 +1,13 @@
+import { History } from 'lucide-react'
+import EstadoPagina from '../componentes/EstadoPagina'
+
+function Historico() {
+  return (
+    <EstadoPagina
+      icon={History}
+      titulo="Histórico de Eventos"
+    />
+  )
+}
+
+export default Historico
