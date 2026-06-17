11a5beb5a001e1a6475884572971d5184459bbb2
clarinha <158226328+claramtenorio@users.noreply.github.com>
2026-06-17 15:38:17 -0300

    Add Dispositivos component with EstadoPagina

---
+++ client/src/paginas/Dispositivos.jsx
@@ -0,0 +1,13 @@
+import { Settings } from 'lucide-react'
+import EstadoPagina from '../componentes/EstadoPagina'
+
+function Dispositivos() {
+  return (
+    <EstadoPagina
+      icon={Settings}
+      titulo="Configurações de Dispositivos"
+    />
+  )
+}
+
+export default Dispositivos
